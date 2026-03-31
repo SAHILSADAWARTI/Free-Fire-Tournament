1bb137345bf3c5f0e26fcae3a91aa5f93430fd99
SAHILSADAWARTI <sahilsadawarti1325@gmail.com>
2026-03-31 17:18:02 +0530

    Update page.tsx

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -18,7 +18,7 @@ const teams = [
 
 export default function TeamsPage() {
   return (
-    <main className="min-h-screen bg-black text-white px-6 py-10">
+    <main className="min-h-screen bg-[url('/teams.jpg')] bg-cover bg-center text-white px-6 py-10">
 
       <h1 className="text-4xl font-bold text-center text-red-500 mb-10">
         TEAMS
