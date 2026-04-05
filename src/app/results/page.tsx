096f675689850515d642b3ba16d0a69f3510e1b1
SAHILSADAWARTI <sahilsadawarti1325@gmail.com>
2026-04-05 09:08:10 +0530

    Update page.tsx

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -30,7 +30,7 @@ const results = [
 
 export default function ResultsPage() {
   return (
-    <main className="min-h-screen bg-[url('/results.jpg')] bg-cover bg-center text-white px-6 py-10">
+    <main className="min-h-screen bg-black bg-cover bg-center text-white px-6 py-10">
 
       <h1 className="text-4xl font-bold text-center text-red-500 mb-10">
         MATCH RESULTS
