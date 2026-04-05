b00d2fb564d7b3c97162961b324fd35752daf6b5
SAHILSADAWARTI <sahilsadawarti1325@gmail.com>
2026-04-05 09:19:57 +0530

    Update page.tsx

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,6 +1,13 @@
 export default function ResultsPage() {
   return (
-    <main className="min-h-screen bg-black text-white px-6 py-10">
+    <main
+  className="min-h-screen text-white px-6 py-10"
+  style={{
+    backgroundImage: "url('/results.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  }}
+>
 
       <h1 className="text-4xl font-bold text-center text-red-500 mb-10">
         MATCH RESULTS
